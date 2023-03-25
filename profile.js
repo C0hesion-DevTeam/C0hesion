2a3fdaf2e882dd9c95f83e505ba8d6c94c5e818f
Percy <105260131+TheCYPER@users.noreply.github.com>
2023-03-25 16:50:53 +0800

    登录修改

--- profile.js
+++ profile.js
@@ -1,16 +1,23 @@
-// pages/personinfo/personinfo.js
+//index.js
+//获取应用实例
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    Name : "",
-    avatarUrl : "",
-    gender : "",
-    province : "",
-    city : "",
-    system : ""
+    motto: '活动让生活更美好!',
+    userInfo: {
+      Name : "",
+      avatarUrl : "",
+      gender : "",
+      province : "",
+      city : "",
+      system : ""
+    }
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
 
   showUserInfoTap:function(){
@@ -45,59 +52,7 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function () {
+    app.getUserInfo(userInfo => this.setData({ userInfo }))
   }
 })
